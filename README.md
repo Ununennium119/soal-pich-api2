7dc971e9e5a9f093c7b7a7ed6212f6a7a19af431
Mohammad Sadeghi <mohammad.s@smartech.ir>
2024-12-30 15:06:41 +0330

    chore: initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# So'al Pich API
+
+Server application (back-end) for the Web Programming course project.
---
+++ src/main/kotlin/com/example/soalpichapi2/SoalPichApi2Application.kt
@@ -0,0 +1,11 @@
+package com.example.soalpichapi2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoalPichApi2Application
+
+fun main(args: Array<String>) {
+    runApplication<SoalPichApi2Application>(*args)
+}
